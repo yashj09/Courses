12dac3888bb7947d37e983010364ffc3eb631ebb
Yash Jain <yashj8858@gmail.com>
2024-06-21 20:54:17 +0530

    fix backend deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://coursesbackend-fbnbxfykr-yash-jains-projects.vercel.app/', // Update with your backend base URL
+  baseURL: 'https://coursesbackend.vercel.app/', // Update with your backend base URL
 });
 
 export default axiosInstance;
